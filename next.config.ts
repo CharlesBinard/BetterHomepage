7791c24248eecf7a17a89728588d2db8b6d5e68c
Charles Binard <charles.binard@gmail.com>
2025-03-02 19:25:46 +0100

    fix config again

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone", // Optimizes for Docker deployments
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: [
       // Add domains for remote images here
